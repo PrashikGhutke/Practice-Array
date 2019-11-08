f3d19097696ae80396866efee52db506d131ed8e
PrashikGhutke <53096500+PrashikGhutke@users.noreply.github.com>
2019-11-08 18:26:33 +0530

    Add files via upload

---
+++ ArrayAsc.java
@@ -0,0 +1,46 @@
+import java.util.Scanner ;
+class ArrayAsc
+{
+	int a[] = new int[100];
+	
+	//Display The Data
+	void show(int []a,int n )
+	{
+		for(int i =0 ;i <n ;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+	}
+	void sortArray(int n)
+	{
+		for(int i = 0 ;i<n ;i++)
+		{
+			for(int j = i +1 ; j < n ; j++)
+			{
+				if(a[i] > a[j])
+				{
+					int temp = a[i] ;
+					a[i] = a[j] ;
+					a[j] = temp ;
+				}
+			}
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		ArrayAsc  obj = new ArrayAsc();
+		System.out.println("Enter the Data");
+		int n = sc.nextInt(); // Size of the Array
+		
+		for(int i = 0 ;i<n ;i++)
+		{
+			obj.a[i]  = sc.nextInt();
+		}
+		obj.show(obj.a,n);
+		obj.sortArray(n);
+		System.out.println();
+		obj.show(obj.a,n);
+	}
+}
